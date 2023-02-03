52bdae8138e5d379150a98fadfd1794ef6ead455
AlexBCIT <asomerville1@my.bcit.ca>
2023-02-03 14:18:11 -0800

    class 5 playwright

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.container} id="colouring">
+      
+      <nav>
+        <Link href="/about">About</Link>
+      </nav>
+    </main>
+  )
+}
